c334a0932ca9414c4e7b80ad5f094c2e4c82c65b
Shivam <shivammauryaw@gmail.com>
2026-03-30 16:56:12 +0530

    Added quiz functionality and question wrapper

--- src/main/java/Shivam/Quiz/App/controller/QuizController.java
+++ src/main/java/Shivam/Quiz/App/controller/QuizController.java
@@ -1,10 +1,14 @@
 package Shivam.Quiz.App.controller;
 
+import Shivam.Quiz.App.model.Question;
+import Shivam.Quiz.App.model.QuestionWrapper;
 import Shivam.Quiz.App.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/quiz")
@@ -16,6 +20,11 @@ public class QuizController {
     @PostMapping("/create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title) {
         return quizService.createQuiz(category, numQ, title);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id) {
+        return quizService.getQuizQuestions(id);
     }
 
 }
--- src/main/java/Shivam/Quiz/App/service/QuizService.java
+++ src/main/java/Shivam/Quiz/App/service/QuizService.java
@@ -3,13 +3,16 @@ package Shivam.Quiz.App.service;
 import Shivam.Quiz.App.dao.QuestionDao;
 import Shivam.Quiz.App.dao.QuizDao;
 import Shivam.Quiz.App.model.Question;
+import Shivam.Quiz.App.model.QuestionWrapper;
 import Shivam.Quiz.App.model.Quiz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -31,5 +34,17 @@ public class QuizService {
         quizDao.save(quiz);
 
         return new ResponseEntity<>("Created", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+        Optional<Quiz> quiz = quizDao.findById(id);
+        List<Question> questionFromDB = quiz.get().getQuestions();
+        List<QuestionWrapper> questionsForUser = new ArrayList<>();
+        for(Question q: questionFromDB) {
+            QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+            questionsForUser.add(qw);
+        }
+
+        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
     }
 }
